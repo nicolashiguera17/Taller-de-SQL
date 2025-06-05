f9620c94715aebff30db04ed14fd2727abb4396e
nicolashiguera17 <189983438+nicolashiguera17@users.noreply.github.com>
2025-06-05 08:35:41 -0500

    feat: :sparkles: exercise eleven

--- Consultas_basicas.sql
+++ Consultas_basicas.sql
@@ -41,3 +41,8 @@ SELECT AVG(salario) from empleados;
 
 --10. Encuentra el número de productos en cada categoría, mostrando la categoría y el número de productos.--
 SELECT categoria, count(*) FROM productos GROUP BY categoria;
+
+--11.Obtén una lista de productos con un precio mayor a $75 USD, mostrando solo el nombre, el precio y su respectivo precio en USD.--
+SELECT nombre,precio/4000 FROM productos
+WHERE (precio/4000)>75;
+

eee52578b79b54cdf99cd98324ac839ef8984ea2
nicolashiguera17 <189983438+nicolashiguera17@users.noreply.github.com>
2025-06-05 07:21:57 -0500

    feat: :sparkles: exercise three

--- Consultas_basicas.sql
+++ Consultas_basicas.sql
@@ -4,3 +4,11 @@ SELECT * FROM usuarios;
 -- 2.Muestra los nombres y correos electrónicos de todos los clientes que residen en la ciudad de Madrid.--
 SELECT nombre,email,ciudad FROM usuarios
 WHERE ciudad = 'Madrid';
+
+--3.Obtén una lista de productos con un precio mayor a $100.000, mostrando solo el nombre y el precio.--
+SELECT nombre,precio from productos
+WHERE precio > 100000;
+
+
+
+
